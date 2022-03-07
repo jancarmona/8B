43999ea1d50339ed9f0dd6f1d76aa167e1d3aee4
jancarmona <100601126+jancarmona@users.noreply.github.com>
2022-03-07 09:56:28 -0600

    primer cambio
    hola mundo y enrutamiento

---
+++ web/app/Views/pages/about.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Primera vista</title>
+  </head>
+  <body>
+    <h1>Hola mundo</h1>
+  </body>
+</html>
